86bb0b59b7a558b7380cc774f16666cd5054f668
HamzaMubashir <mubashir.m@northeastern.edu>
2025-08-06 09:13:14 -0400

    grammerly causing hydration warnign which are now suppressed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
+        suppressHydrationWarning={true}
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
