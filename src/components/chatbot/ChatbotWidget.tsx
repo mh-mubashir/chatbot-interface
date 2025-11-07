e0205581b945c6a0976be49826714aaa1b434ecd
HamzaMubashir <mubashir.m@northeastern.edu>
2025-11-06 23:10:14 -0500

    resolved the link appearence issue

--- src/components/chatbot/ChatbotWidget.tsx
+++ src/components/chatbot/ChatbotWidget.tsx
@@ -49,9 +49,9 @@ const ChatbotWidget: React.FC = () => {
             href={part}
             target="_blank"
             rel="noopener noreferrer"
-            className="text-blue-600 hover:text-blue-800 underline break-all"
+            className="text-blue-600 hover:text-blue-800 underline"
           >
-            {part}
+            Link
           </a>
         );
       }
